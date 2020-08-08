6ac890a1df2e516f0220b5a3c708583cb140e5f8
Roland Clark <rolandclrk@icloud.com>
2020-08-07 22:23:24 -0400

    Commit

--- Source Control/ViewController.swift
+++ Source Control/ViewController.swift
@@ -15,7 +15,15 @@ class ViewController: NSViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    /**
+     Adds two numbers togeather and returns result.
+     - parameter num1: Ther first number.
+     - parameter num2: The second number.
+     - returns: The sum of num1 and num2.
+     */
+    func addNumbers(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
